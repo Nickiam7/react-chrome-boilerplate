da9dfaaecdf6056b97581b0ba2bac051c9e46834
nmcneany <nmcneany@gmail.com>
2024-07-21 13:20:14 -0500

    options page

---
+++ src/pages/Options.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Options = () => {
+  return(
+    <div>
+      <p>Hello from <strong>'src/pages/Options.jsx'</strong></p>
+    </div>
+  )
+}
+
+export default Options
